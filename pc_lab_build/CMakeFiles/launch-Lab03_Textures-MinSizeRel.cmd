3a6f849a474ebd85f8d71cc7cab0c8d7a7588ccc
Jon Shiach <jon.shiach@gmail.com>
2024-02-12 17:55:36 +0000

    Added pre-built project files for PC labs

---
+++ pc_lab_build/CMakeFiles/launch-Lab03_Textures-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/55093290/OneDrive - MMU/Documents/Lab03_Textures/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\55093290\OneDrive - MMU\Documents\Lab03_Textures\build\MinSizeRel\Lab03_Textures.exe"  
+pause
