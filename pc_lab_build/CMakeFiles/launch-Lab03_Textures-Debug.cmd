214710c4a77459d428ce2bc5512e299fa5207ea5
Jon Shiach <60979354+jonshiach@users.noreply.github.com>
2024-02-13 17:06:15 +0000

    Delete pc_lab_build directory

--- pc_lab_build/CMakeFiles/launch-Lab03_Textures-Debug.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/55093290/OneDrive - MMU/Documents/Lab03_Textures/source/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\55093290\OneDrive - MMU\Documents\Lab03_Textures\build\Debug\Lab03_Textures.exe"  
-pause
